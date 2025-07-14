16fb98627831eb70d686648bab68428807976a1a
beniirawan31 <beni31irawan05@gmail.com>
2025-07-15 02:23:04 +0700

    menu2

---
+++ resources/views/layout/user/pricing.blade.php
@@ -0,0 +1,134 @@
+<!-- ======= Features =======-->
+    <section class="section features__v2" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-lg-flex p-5 rounded-4 content" data-aos="fade-in" data-aos-delay="0">
+                        <div class="row">
+                            <div class="col-lg-5 mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="0">
+                                <div class="row">
+                                    <div class="col-lg-11">
+                                        <div class="h-100 flex-column justify-content-between d-flex">
+                                            <div>
+                                                <h2 class="mb-4">Why Choose us</h2>
+                                                <p class="mb-5">Experience the future of finance with our
+                                                    secure, efficient, and user-friendly financial services. Our
+                                                    cutting-edge platform ensures your transactions are safe,
+                                                    streamlined, and easy to manage, empowering you to take
+                                                    control of your financial journey with confidence and
+                                                    convenience."</p>
+                                            </div>
+                                            <div class="align-self-start"><a
+                                                    class="glightbox btn btn-play d-inline-flex align-items-center gap-2"
+                                                    href="https://www.youtube.com/watch?v=DQx96G4yHd8"
+                                                    data-gallery="video"><i class="bi bi-play-fill"></i> Watch
+                                                    the Video</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="row justify-content-end">
+                                    <div class="col-lg-11">
+                                        <div class="row">
+                                            <div class="col-sm-6" data-aos="fade-up" data-aos-delay="0">
+                                                <div class="icon text-center mb-4"><i class="bi bi-person-check fs-4"></i>
+                                                </div>
+                                                <h3 class="fs-6 fw-bold mb-3">User-Friendly Interface</h3>
+                                                <p>Easy navigation with responsive design for various devices.
+                                                </p>
+                                            </div>
+                                            <div class="col-sm-6" data-aos="fade-up" data-aos-delay="100">
+                                                <div class="icon text-center mb-4"><i class="bi bi-graph-up fs-4"></i>
+                                                </div>
+                                                <h3 class="fs-6 fw-bold mb-3">Financial Analytics</h3>
+                                                <p>Budget tracking, expense categorization, and personalized
+                                                    insights.</p>
+                                            </div>
+                                            <div class="col-sm-6" data-aos="fade-up" data-aos-delay="200">
+                                                <div class="icon text-center mb-4"><i class="bi bi-headset fs-4"></i>
+                                                </div>
+                                                <h3 class="fs-6 fw-bold mb-3">Customer Support</h3>
+                                                <p>24/7 service via chat, email, phone, and a detailed help
+                                                    center.</p>
+                                            </div>
+                                            <div class="col-sm-6" data-aos="fade-up" data-aos-delay="300">
+                                                <div class="icon text-center mb-4"><i class="bi bi-shield-lock fs-4"></i>
+                                                </div>
+                                                <h3 class="fs-6 fw-bold mb-3">Security Features</h3>
+                                                <p>Data encryption, fraud detection, and prevention mechanisms.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Features-->
+
+    <!-- ======= Pricing =======-->
+    <section class="section pricing__v2" id="pricing">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md-5 mx-auto text-center"><span class="subtitle text-uppercase mb-3" data-aos="fade-up"
+                        data-aos-delay="0">Pricing</span>
+                    <h2 class="mb-3" data-aos="fade-up" data-aos-delay="100">Plan for every budget</h2>
+                    <p data-aos="fade-up" data-aos-delay="200">Experience the future of finance with our
+                        secure, efficient, and user-friendly financial services</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 mb-4 mb-md-0" data-aos="fade-up" data-aos-delay="300">
+                    <div class="p-5 rounded-4 price-table h-100">
+                        <h3>Personal</h3>
+                        <p>Choose a plan that fits your personal financial needs and start managing your
+                            finances more effectively.</p>
+                        <div class="price mb-4"><strong>$7</strong><span>/ month</span></div>
+                        <div><a class="btn" href="#">Get Started</a></div>
+                    </div>
+                </div>
+                <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+                    <div class="p-5 rounded-4 price-table popular h-100">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h3 class="mb-3">Business</h3>
+                                <p>Optimize your business financial operations with our tailored business plans.
+                                </p>
+                                <div class="price mb-4"><strong class="me-1">$29</strong><span>/
+                                        month</span></div>
+                                <div><a class="btn btn-white hover-outline" href="#">Get Started</a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 pricing-features">
+                                <h4 class="text-uppercase fw-bold mb-3">Features</h4>
+                                <ul class="list-unstyled d-flex flex-column gap-3">
+                                    <li class="d-flex gap-2 align-items-start mb-0"><span
+                                            class="icon rounded-circle position-relative mt-1"><i
+                                                class="bi bi-check"></i></span><span>Personalized financial
+                                            insights and reports</span></li>
+                                    <li class="d-flex gap-2 align-items-start mb-0"><span
+                                            class="icon rounded-circle position-relative mt-1"><i
+                                                class="bi bi-check"></i></span><span>Priority customer
+                                            support</span></li>
+                                    <li class="d-flex gap-2 align-items-start mb-0"><span
+                                            class="icon rounded-circle position-relative mt-1"><i
+                                                class="bi bi-check"></i></span><span>Access to exclusive
+                                            investment opportunities</span></li>
+                                    <li class="d-flex gap-2 align-items-start mb-0"><span
+                                            class="icon rounded-circle position-relative mt-1"><i
+                                                class="bi bi-check"></i></span><span>AI-driven financial
+                                            recommendations</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Pricing-->
